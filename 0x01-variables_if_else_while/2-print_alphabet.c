96e7b184f7a72357c244e43be27e4a91a90d5052
Joshua Ochoyi <joshuajrochoyi@gmail.com>
2023-02-21 13:56:46 -0800

    editied

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,20 +15,3 @@ int main(void)
 
 return (0);
 }
-
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
-	return (0);
-}
