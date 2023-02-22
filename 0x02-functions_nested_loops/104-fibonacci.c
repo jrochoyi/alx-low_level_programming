cb684aa66556156b376ba99281127e8e527102e5
Joshua Ochoyi <joshuajrochoyi@gmail.com>
2023-02-21 23:45:16 -0800

    104 fib task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #define LARGEST 10000000000
-
 /**
- * main - Entry point
- *
- * Desc: Finds and prints the first 98 Fibonacci numbers.
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
  */
 int main(void)
 {
-	int i;
-	unsigned int fib1 = 1, fib2 = 2, next_fib;
-	printf("%u, %u", fib1, fib2);
-	for (i = 2; i < 98; i++)
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
+
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		next_fib = fib1 + fib2;
-		printf(", %u", next_fib);
-		fib1 = fib2;
-		fib2 = next_fib;
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
 	printf("\n");
 	return (0);
