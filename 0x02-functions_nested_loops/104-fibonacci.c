710def3590d3f6bb8e878d10402c56c3c92c3518
Joshua Ochoyi <joshuajrochoyi@gmail.com>
2023-02-21 23:27:29 -0800

    104-Fib task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Finds and prints the first 98 Fibonacci numbers, starting with 1 and 2.
+ * Description: Finds and prints the first 98 Fibonacci numbers, starts with 1 and 2
  *
  * Return: Always 0 (Success)
  */
